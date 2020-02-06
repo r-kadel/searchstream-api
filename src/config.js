5da5a2e79af05e6e42888caf23c4f17c32bb548e
Ryan Kadel <ryan.kadel88@gmail.com>
2020-02-06 08:05:21 -0500

    link with FE

--- src/config.js
+++ src/config.js
@@ -2,5 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     CLIENT_ORIGIN: 'https://searchstream.r-kadel.now.sh/',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/searchstream'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/searchstream',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
+    "http://localhost:3000/api"
   }
