92f1edf39e02a91e0f81f9d192c9e631674a1812
Ryan Kadel <ryan.kadel88@gmail.com>
2020-02-06 07:32:53 -0500

    users endpoint set up

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const { CLIENT_ORIGIN } = require('./config')
 const errorHandler = require('./error-handler')
+const usersRouter = require('./users/users-router')
 
 const app = express()
 
@@ -13,17 +14,18 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common'
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
-
-app.get('/api/*', (req, res) => {
-  res.send('Hello, world!')
-})
-
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
 )
+
+app.get('/api', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.use('/api/users', usersRouter)
+
 
 app.use(errorHandler)
 
