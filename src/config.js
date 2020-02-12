75b4b1705fa855f08eb931c669407d0fd2560f3f
Ryan Kadel <ryan.kadel88@gmail.com>
2020-02-12 07:06:40 -0500

    update

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: 'https://searchstream.r-kadel.now.sh/',
+    CLIENT_ORIGIN: 'https://searchstream.r-kadel.now.sh' || 'http://localhost:3000',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/searchstream',
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
     "http://localhost:3000/api"
   }
+
+  'https://searchstream.r-kadel.now.sh/'
